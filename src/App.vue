01312c9a089499aa34b74e3b47686b0af88b3ad3
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-04-02 15:38:29 +0200

    add thumbnails

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList :videos="videos"></VideoList>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import SearchBar from './components/SearchBar.vue';
+import VideoList from './components/VideoList.vue';
+const API_KEY = 'AIzaSyBTEQmwXJUPsYXJ1mzf7EvK3go8CGJCk_U';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App', // need mostly for debugging
+    components: {
+        SearchBar,
+        VideoList
+    },
+    data(){
+        return{
+            videos: []
+        }
+    },
+    methods: {
+        onTermChange(searchTerm){
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            })
+            .then(response => {
+                this.videos = response.data.items;
+            });
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul class="list-group">
+        <VideoListItem 
+            v-for="myVideo in videos" 
+            :key="myVideo.etag"
+            :video="myVideo">
+        </VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem.vue';
+
+export default {
+    name: 'VideoList',
+    components: {
+        VideoListItem
+    },
+    props: {
+        videos: Array
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
